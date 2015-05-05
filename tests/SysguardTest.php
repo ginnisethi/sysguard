7c816894268e5ffa322e64c6d9828b8e5edb170b
Ifan Iqbal <ifaniqbal.com@gmail.com>
2015-05-05 14:48:41 +0700

    Added: tests for effective menu, effective permission, and hierarchical menu.

--- tests/SysguardTest.php
+++ tests/SysguardTest.php
@@ -4,6 +4,7 @@ use Illuminate\Auth\EloquentUserProvider;
 use Ifaniqbal\Sysguard\User;
 use Ifaniqbal\Sysguard\Group;
 use Ifaniqbal\Sysguard\Permission;
+use Ifaniqbal\Sysguard\Menu;
 
 class SysguardTest extends Orchestra\Testbench\TestCase {
     
@@ -11,7 +12,6 @@ class SysguardTest extends Orchestra\Testbench\TestCase {
     {
         parent::setUp();
         Artisan::call('vendor:publish');
-        Artisan::call('migrate');
     }
 
     protected function getPackageProviders()
@@ -48,6 +48,7 @@ class SysguardTest extends Orchestra\Testbench\TestCase {
      */
     public function testService()
     {
+        Artisan::call('migrate');
         $this->assertEquals(true, Auth::guest());
 
         $user = User::create(
@@ -91,5 +92,118 @@ class SysguardTest extends Orchestra\Testbench\TestCase {
         );
 
         $this->assertEquals(true, $sysguard->authorize());
+    }
+
+    /**
+     * Test hierarchical menu that can be used in sidebar.
+     */
+    public function testHierarchicalMenu()
+    {
+        Artisan::call('migrate');
+
+        $user = User::create([
+            'name'      => 'Ifan Iqbal',
+            'email'     => 'ifaniqbal.com@gmail.com',
+            'password'  => 'secret'
+        ]);
+
+        $group = Group::create([
+            'name' => 'admin'
+        ]);
+
+        for ($i = 0; $i < 2; $i++) {
+            $menu = Menu::create([
+                'parent_id' => 0,
+                'name' => 'Menu ' . $i + 1,
+                'order' => $i + 1,
+                'enabled' => 1,
+            ]);
+
+                Menu::create([
+                    'parent_id' => $menu->id,
+                    'name' => 'Child 1',
+                    'order' => 1,
+                    'enabled' => 1,
+                ]);
+
+                Menu::create([
+                    'parent_id' => $menu->id,
+                    'name' => 'Child 2',
+                    'order' => 2,
+                    'enabled' => 1,
+                ]);
+        }
+
+        $menus = Menu::get();
+        $user->groups()->attach($group);
+        $group->menus()->sync($menus);
+        
+
+        Auth::loginUsingId($user->id);
+
+        $hierarchicalMenu = Sysguard::getHierarchicalMenu();
+
+        $this->assertEquals(2, $hierarchicalMenu->count());
+        $this->assertEquals(2, $hierarchicalMenu[0]->children->count());
+        $this->assertEquals('Child 2', $hierarchicalMenu[0]->children[1]->name);
+    }
+
+    /**
+     * Test effective menu and permission
+     */
+    public function testEffectiveItems()
+    {
+        Artisan::call('migrate');
+
+        $user = User::create([
+            'name'      => 'Ifan Iqbal',
+            'email'     => 'ifaniqbal.com@gmail.com',
+            'password'  => 'secret'
+        ]);
+
+        $group = Group::create([
+            'name' => 'admin'
+        ]);
+
+        for ($i = 0; $i < 2; $i++) {
+            $menu = Menu::create([
+                'parent_id' => 0,
+                'name' => 'Menu ' . $i + 1,
+                'order' => $i + 1,
+                'enabled' => 1,
+            ]);
+
+                Menu::create([
+                    'parent_id' => $menu->id,
+                    'name' => 'Child 1',
+                    'order' => 1,
+                    'enabled' => 1,
+                ]);
+
+                Menu::create([
+                    'parent_id' => $menu->id,
+                    'name' => 'Child 2',
+                    'order' => 2,
+                    'enabled' => 1,
+                ]);
+        }
+
+        for ($i = 0; $i < 4; $i++) {
+            Permission::create([
+                'route' => 'permission_' . $i + 1,
+                'enabled' => 1,
+            ]);
+        }
+
+        $menus = Menu::get();
+        $permissions = Permission::get();
+        $user->groups()->attach($group);
+        $group->menus()->sync($menus);
+        $group->permissions()->sync($permissions);        
+
+        Auth::loginUsingId($user->id);
+
+        $this->assertEquals(6, Sysguard::getEffectiveMenu()->count());
+        $this->assertEquals(4, Sysguard::getEffectivePermission()->count());
     }
 }
