fbdff51b1e1775262ba09562d28548d118bc9f20
Ifan Iqbal <ifaniqbal.com@gmail.com>
2015-05-05 14:48:08 +0700

    Added: implementation of effective menu, effective permission, and hierarchical menu.

--- src/Sysguard.php
+++ src/Sysguard.php
@@ -6,7 +6,11 @@ use Illuminate\Routing\Router;
 class Sysguard {
 
     private $auth;
+
     private $router;
+
+    private $effectiveMenu = null;
+    private $hierarchicalMenu = null;
 
     public function __construct(GuardContract $auth, Router $router)
     {
@@ -30,16 +34,46 @@ class Sysguard {
         return $this->authorizeByRoute($route);
     }
 
-    public function effectiveMenus()
+    public function getEffectiveMenu()
     {
+        return $this->auth->user()
+            ->getActiveGroup()
+            ->menus()
+            ->where('enabled', 1)
+            ->get();
     }
 
-    public function effectivePermissions()
+    public function getEffectivePermission()
     {
+        return $this->auth->user()
+            ->getActiveGroup()
+            ->permissions()
+            ->where('enabled', 1)
+            ->get();
     }
 
-    public function hierarchicalMenu()
+    public function getHierarchicalMenu()
     {
+        if ($this->hierarchicalMenu == null) {
+            $activeGroup = $this->auth->user()->getActiveGroup();
+
+            $hierarchicalMenu = [];
+            if ($activeGroup != null) {
+                $hierarchicalMenu = $activeGroup->menus()
+                    ->with(['children' => function($query)
+                        {
+                            $query->orderBy('order');
+                        }])
+                    ->where('parent_id', 0)
+                    ->where('enabled', 1)
+                    ->orderBy('order')
+                    ->get();
+            }
+
+            $this->hierarchicalMenu = $hierarchicalMenu;
+        }
+
+        return $this->hierarchicalMenu;
     }
 
 }
