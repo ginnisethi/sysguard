9c4560951a02912eec3950a4498a7d4509d50767
Ifan Iqbal <ifaniqbal.com@gmail.com>
2015-05-05 14:46:57 +0700

    Added: parent and children relations. Fixed: typo on fillable property.

--- src/Menu.php
+++ src/Menu.php
@@ -4,9 +4,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Menu extends Model {
 
-    protected $fillable = ['parent_id', 'name', 'url', 'icon', 'order', 'enabled']];
+    protected $fillable = ['parent_id', 'name', 'url', 'icon', 'order', 'enabled'];
 
     public function groups() {
         return $this->belongsToMany('Ifaniqbal\Sysguard\Group');
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo('Ifaniqbal\Sysguard\Menu', 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany('Ifaniqbal\Sysguard\Menu', 'parent_id');
     }
 }
