691d378b6e0db0ed5c5eede4573bb906532bf5cd
Ifan Iqbal <ifaniqbal.com@gmail.com>
2015-05-28 18:28:56 +0700

    Minimize style and layout

--- src/views/partials/head.blade.php
+++ src/views/partials/head.blade.php
@@ -1,15 +1,6 @@
 <meta charset="UTF-8">
-<title></title>
+<title>Sysguard Authorize</title>
 <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
 <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-<!-- Bootstrap 3.3.2 -->
-<link href="{{ asset('/adminlte/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />    
-<!-- FontAwesome 4.3.0 -->
-<link href="{{ asset('/font-awesome-4.3.0/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css" />
-<!-- Theme style -->
-<link href="{{ asset('/adminlte/dist/css/AdminLTE.min.css') }}" rel="stylesheet" type="text/css" />
-<!-- AdminLTE Skins. Choose a skin from the css/skins 
-     folder instead of downloading all of them to reduce the load. -->
-<link href="{{ asset('/adminlte/dist/css/skins/skin-blue.min.css') }}" rel="stylesheet" type="text/css" />
-<!-- jQuery 1.11.2 -->
-<script src="{{ asset('/js/jquery-1.11.2.min.js') }}"></script>
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet" type="text/css" />    
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
